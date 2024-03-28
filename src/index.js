83ecbc73591295ec23c07db2418a6bd1675b4b1a
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-03-29 05:20:22 +0900

    feat: FooterコンポーネントとRightコンポーネントの使用

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './Header';
+import Footer from './Footer';
+import Right from './Right';
 import './font.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
+    <Footer/>
+    <Right/>
 
 
   </React.StrictMode>
