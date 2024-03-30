02054242959e083a372cb5ee98341cc4a0e11241
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-03-30 14:46:32 +0900

    [refactor]: JSX用に属性を修正
    - class -> className

--- src/Footer.js
+++ src/Footer.js
@@ -9,19 +9,23 @@ export default function Fotter() {
 
     return (
         <>
-        <footer class="footer">
-            <ul className="menu_nav">
-                <li class="menu_navItem"><a href="">HOME</a></li>
-                <li class="menu_navItem"><a href="">ACTIVITES</a></li>
-                <li class="menu_navItem"><a href="">NEWS</a></li>
-                <li class="menu_navItem"><a href="">JOIN</a></li>
-                <li class="menu_navItem"><a href="">CONTACT</a></li>
-            </ul>
-            <div class="links">
-                <h3 class="x_logo"><a href={x_path}><ReadImage src={logo_path} alt="Xのロゴ" /></a></h3>
-                <div class="ait_link"><a href={ait_path}>AICHI INSTITUTE OF TECHNOLOGY</a></div>
-            </div>
-        </footer>
+            <footer className="footer">
+                <ul className="menu_nav">
+                    <li className="menu_navItem"><a href="./">HOME</a></li>
+                    <li className="menu_navItem"><a href="./">ACTIVITES</a></li>
+                    <li className="menu_navItem"><a href="./">NEWS</a></li>
+                    <li className="menu_navItem"><a href="./">JOIN</a></li>
+                    <li className="menu_navItem"><a href="./">CONTACT</a></li>
+                </ul>
+                <div className="links">
+                    <h3 className="x_logo">
+                        <a href={x_path}>
+                            <ReadImage src={logo_path} alt="Xのロゴ" />
+                        </a>
+                    </h3>
+                    <div className="ait_link"><a href={ait_path}>AICHI INSTITUTE OF TECHNOLOGY</a></div>
+                </div>
+            </footer>
         </>
 
     );
