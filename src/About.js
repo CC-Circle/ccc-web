f9fa5bff7a0803c2f90ccfee6f5f67474ddc36ba
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-04-01 17:19:57 +0900

    [feat]: Aboutコンポーネントの追加

---
+++ src/About.js
@@ -0,0 +1,18 @@
+export default function About({ }) {
+    return (
+        <div className="bg-gray-700 w-full flex">
+            <div className="" style={{textAlign: 'center', float: 'left', width: '20%'}}>
+                <div className="" style={{}}>
+                    About
+                </div>
+                <div className="" style={{}}>ContentsCreationCircleについて</div>
+            </div>
+            <div className="" style={{width:'80%',float:'right'}}>
+                ContentsCreationCircleは、愛知工業大学八草キャンパスを拠点として活動している 公認サークルです。<br />
+                2023年10月に創設された新しいサークルです。<br />
+                コンテンツや作品をつくることを目的をとして、それぞれが持ち寄ったアイデアや技術、知識などを活用し 新しいものを生み出していきます。  基本的にはチーム開発をすることで進めていき、同じ目標を持った仲間をともにプロジェクトを進めていきます。
+            </div>
+
+        </div>
+    );       
+}
