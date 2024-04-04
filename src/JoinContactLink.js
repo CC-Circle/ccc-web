ddbfb2b274a8cdd6bf062dd60716f5f0491a6c5e
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-04-04 21:10:38 +0900

    [feat]:Joinコンポーネントの追加

---
+++ src/JoinContactLink.js
@@ -0,0 +1,10 @@
+export default function HomeJoinContact() {
+    return (
+        <div className="flex text-center  justify-center mb-[96px]">
+            <a href="./contact" className="border border-[#000] w-[30%] h-auto py-[5%] hover:text-[#fff] hover:bg-[#999] hover:border-[#999] hover:transition-all hover:duration-[0.5s] hover:ease-[ease-in-out]">
+                <div className="font-[Museno-900] text-[32px]">CONTACT</div>
+                <div className="font-[Noto-Sans-Regular] text-[12px] ">参加について問い合わせ</div>
+            </a>
+        </div>
+    );
+}
