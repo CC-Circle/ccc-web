218a63b685cac7f010d2367c2e3dc3ae4a4a28b4
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-04-08 17:42:46 +0900

    [feat]:テキストフェードアニメーションのためのコンポーネント実装

---
+++ src/FadeInBottom.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useInView } from "react-intersection-observer";
+
+export default function FadeInBottom({ children }) {
+    const { ref, inView } = useInView({
+      // ref要素が現れてから50px過ぎたら
+      rootMargin: "-50px",
+      // 最初の一度だけ実行
+      triggerOnce: true,
+    });
+  
+    const fadeInClassName = inView ? "animate-fade-in-bottom" : "opacity-0";
+  
+    const wrappedChildren = React.Children.map(children, child => {
+      if (React.isValidElement(child)) {
+        const className = [child.props.className, fadeInClassName]
+          .filter(el => el)
+          .join(" ");
+  
+        return React.cloneElement(child, {
+          ref,
+          className,
+        });
+      } else {
+        return child;
+      }
+    });
+  
+    return <>{wrappedChildren}</>;
+}
