9cea959b0f55af401757442aec0ba2a30b814b96
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-04-08 16:09:16 +0900

    [feat]:hr画像の削除

--- src/Hr.js
+++ src/Hr.js
@@ -3,6 +3,8 @@ import ReadImage from './ReadImage';
 export default function Hr() {
     const image_path = "./image/logo2.png";
     return (
-            <ReadImage className={"md:w-[100px] w-[50px] md:my-[96px] my-[48px] md:mx-[calc(50%-50px)] mx-[calc(50%-25px)]"} src={image_path} alt="hr"/>
+        <div className='md:w-[100px] w-[50px] md:my-[96px] my-[48px] md:mx-[calc(50%-50px)] mx-[calc(50%-25px)]'>
+            {/* <ReadImage className={"md:w-[100px] w-[50px] md:my-[96px] my-[48px] md:mx-[calc(50%-50px)] mx-[calc(50%-25px)]"} src={image_path} alt="hr"/> */}
+        </div>
     );
 }
