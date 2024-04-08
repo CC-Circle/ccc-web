67517e5e33f10134d209d372115cec724c6ceedf
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-04-08 17:42:03 +0900

    [feat]:react-intersection-observerのインストール

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
-    content: [
-  
-      "./src/**/*.{js,jsx,ts,tsx}",
-  
-    ],
-  
-    theme: {
-      extend: {},
+  content: [
+
+    "./src/**/*.{js,jsx,ts,tsx}",
+
+  ],
+
+  theme: {
+    extend: {
+      animation: {
+        "fade-in-bottom": "fade-in-bottom 0.6s ease-out   both",
+      },
+      keyframes: {
+        "fade-in-bottom": {
+          "0%": {
+            transform: "translateY(50px)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
