9d45623a624e43d30a5a4e7586f48da68cd20650
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-04-10 14:30:49 +0900

    [feat]:Google Search Console to Netlify

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <meta name="google-site-verification" content="0tBPXSCGNIISV-DFNVH-HLgVb0PaHhtrroZS03TL7ag" />
       <Head />
       <body>
         <Main />
