d4d778bc9197e08c3eded77fc7c3c5574ab58164
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-04-11 14:02:25 +0900

    [feat]:サイトタイトルとロゴの設定

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>コンテンツクリエーションサークル</title>
+      <link rel="icon" type="image/x-icon" href="./image/favicon.ico"/>
       <Head />
       <body>
         <Main />
